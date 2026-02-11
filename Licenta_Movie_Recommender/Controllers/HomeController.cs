645d8e38e34344d4f000a010159da8c31cc614ba
mariavasilache <mariavasilache777@gmail.com>
2026-02-11 18:07:46 +0200

    Add project files.

---
+++ Licenta_Movie_Recommender/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Licenta_Movie_Recommender.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Licenta_Movie_Recommender.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Licenta_Movie_Recommender/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Licenta_Movie_Recommender.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
